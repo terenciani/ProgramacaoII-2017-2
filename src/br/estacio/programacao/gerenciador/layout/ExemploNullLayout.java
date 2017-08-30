1dc9dac01b7208d49573de2b975ed812e4df98a8
Marcelo <Marcelo@DESKTOP-L81BOPU>
2017-08-29 21:29:12 -0300

    Null Layout

---
+++ src/br/estacio/programacao/gerenciador/layout/ExemploNullLayout.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.estacio.programacao.gerenciador.layout;
+
+import java.awt.Container;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author Marcelo
+ */
+public class ExemploNullLayout extends JFrame {
+
+    private JButton botaoUm, botaoDois, botaoTres;
+
+    public ExemploNullLayout(){
+        Container caixaPrincipal = this.getContentPane();
+        
+        this.setLayout(null);
+        botaoUm = new JButton("Estacio");
+        botaoDois = new JButton("Programação II");
+        botaoTres = new JButton("2017-2");
+        
+        botaoUm.setBounds(10, 10, 150, 140);
+        botaoDois.setBounds(10, 155, 150, 140);
+        botaoTres.setBounds(10, 300, 150, 140);
+        
+        caixaPrincipal.add(botaoUm);
+        caixaPrincipal.add(botaoDois);
+        caixaPrincipal.add(botaoTres);
+        
+        this.setSize(200, 200);
+        this.setVisible(true);
+    }
+}
